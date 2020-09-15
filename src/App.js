5f7b6c8eed2916c052a508b441fa48978945d27b
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-09-15 16:54:21 +0800

    Figuring out how to do tiling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 function App() {
   return (
-    <main>
-      <div className="title">
-        <div className="title-inner">
-          <strong>风</strong>的<br/>重构
+    <main className="home">
+      <div className="tile">
+        <div className="title">
+          <div className="title-inner">
+            <strong>风</strong>的<br/>重构
+          </div>
         </div>
+
+        <div className="tile-meta"><strong>logo</strong>(primary) @ /</div>
       </div>
     </main>
   );
