09386eee7d7ce6e80bdfc614d8bef675647cf571
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-09-17 03:51:10 +0800

    Extracting nav into its own component

---
+++ src/comps/Nav.js
@@ -0,0 +1,19 @@
+// This is not an standalone route
+
+import React from 'react';
+
+const Nav = () => <>
+  <nav>
+    <div className="tile">
+      <div className="title">
+        <div className="title-inner">
+          <strong>风</strong>的<br/>重构
+        </div>
+      </div>
+
+      <div className="tile-meta"><strong>logo</strong>(primary) @ /</div>
+    </div>
+  </nav>
+</>;
+
+export default Nav;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Nav from '../comps/Nav';
 
 import Home from './Home';
 
 const Root = () => <>
-  <nav>
-    <div className="tile">
-      <div className="title">
-        <div className="title-inner">
-          <strong>风</strong>的<br/>重构
-        </div>
-      </div>
-
-      <div className="tile-meta"><strong>logo</strong>(primary) @ /</div>
-    </div>
-  </nav>
-
+  <Nav />
   <Route path="/" exact component={Home} />
 </>;
 
