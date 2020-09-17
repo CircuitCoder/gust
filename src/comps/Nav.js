d9698b33ea757c059067d900f0fb597d59983284
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-09-17 16:42:35 +0800

    Back indicator

--- src/comps/Nav.js
+++ src/comps/Nav.js
@@ -14,8 +14,8 @@ const Nav = () => {
 
   return (
     <nav className={remote ? 'mini' : ''}>
-      <div className="title-tile">
-        <div className="title" onClick={home}>
+      <div className="title-tile" onClick={home}>
+        <div className="title">
           <div className="title-inner">
             <strong>风</strong>的<br />
             重构
@@ -25,6 +25,10 @@ const Nav = () => {
         <div className="title-tile-meta">
           <div className="title-tile-meta-inner">
             <strong>logo</strong>(primary) @ /
+          </div>
+
+          <div className="title-tile-meta-inner title-tile-meta-alt">
+            <strong>BACK</strong>&nbsp;<span class="title-tile-meta-alt-arrow">&gt;</span>
           </div>
         </div>
       </div>
