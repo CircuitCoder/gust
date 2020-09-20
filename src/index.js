ced3e49394f5d2f055b7aa0ecbc8fa9f2f5bb1f5
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-09-21 01:40:12 +0800

    Startup ease

--- src/index.js
+++ src/index.js
@@ -27,6 +27,8 @@ const App = () => (
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+document.body.classList.add('loaded');
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
