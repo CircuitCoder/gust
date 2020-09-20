b4a2e2607d7f6e9f1e3e3fcf4013678da3545eaf
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-09-20 09:00:34 +0800

    Properly calculate sticked left for entry titles. Closes #3.

--- src/comps/Main.js
+++ src/comps/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import clsx from 'clsx';
 
 const Main = ({ on, children, hitzone, ...rest }) => {
@@ -6,8 +6,27 @@ const Main = ({ on, children, hitzone, ...rest }) => {
     'main-wrapper-off': !on,
   });
 
+  const ob = useMemo(
+    () =>
+      new ResizeObserver(entries => {
+        const ent = entries[0];
+        ent.target.style.setProperty('--full-width', ent.contentRect.width + 'px');
+        ent.target.style.setProperty('--full-height', ent.contentRect.height + 'px');
+      }),
+    [],
+  );
+
+  const attach = useCallback(
+    el => {
+      ob.disconnect();
+      if (!el) return;
+      ob.observe(el);
+    },
+    [ob],
+  );
+
   return (
-    <div className={cn}>
+    <div className={cn} ref={attach}>
       {hitzone && <div className="hitzone" ref={hitzone} />}
       <main {...rest}>{children}</main>
     </div>
