e7f4293200d2487a26db710abdf9dc780c2e7a19
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-09-20 20:24:16 +0800

    Fixing read_hash.sh

--- .github/read_hash.sh
+++ .github/read_hash.sh
@@ -8,8 +8,8 @@ export REACT_APP_GEN_REV=$(git rev-parse HEAD)
 cd ../gust-data
 export REACT_APP_SRC_REV=$(git rev-parse HEAD)
 
-echo "::set-env REACT_APP_WEB_REV::$REACT_APP_WEB_REV"
-echo "::set-env REACT_APP_GEN_REV::$REACT_APP_GEN_REV"
-echo "::set-env REACT_APP_SRC_REV::$REACT_APP_SRC_REV"
+echo "::set-env name=REACT_APP_WEB_REV::$REACT_APP_WEB_REV"
+echo "::set-env name=REACT_APP_GEN_REV::$REACT_APP_GEN_REV"
+echo "::set-env name=REACT_APP_SRC_REV::$REACT_APP_SRC_REV"
 
 cd ..
