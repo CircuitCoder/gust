731232343303a3cedded28959e23a0359c46df73
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-09-20 19:13:32 +0800

    Images and raw HTML

--- src/comps/Markdown.js
+++ src/comps/Markdown.js
@@ -1,26 +1,47 @@
 import React from 'react';
 
 import unified from 'unified';
-import parse from 'remark-parse';
+import remark from 'remark-parse';
+import raw from 'rehype-raw';
 import frontmatter from 'remark-frontmatter';
+import attr from 'remark-attr';
 import remark2rehype from 'remark-rehype';
 import rehype2react from 'rehype-react';
+import { DATA_PATH } from '../config';
 
 const processor = unified()
-  .use(parse, { commonmark: true })
+  .use(remark, { commonmark: true })
   .use(frontmatter)
-  .use(remark2rehype)
+  .use(attr)
+  .use(remark2rehype, {
+    allowDangerousHtml: true,
+  })
+  .use(raw)
   .use(rehype2react, {
     createElement: React.createElement,
     Fragment: ({ children }) => <div className="md">{children}</div>,
     components: {
-      h1: ({ children }) => <h1>{ children }</h1>
+      h1: ({ children }) => <h1>{children}</h1>,
+      img: ({ src, alt, ...rest }) => {
+        let normalized_src = src;
+        while (normalized_src.charAt(0) === '/')
+          normalized_src = normalized_src.substr(1);
+        if (!normalized_src.match(/^https:?\/\//))
+          normalized_src = `${DATA_PATH}/${normalized_src}`;
+
+        return (
+          <div className="md-img">
+            <img src={normalized_src} alt={alt} {...rest} />
+            <div className="md-img-alt">{alt}</div>
+          </div>
+        );
+      },
     },
   })
   .freeze();
 
 const Markdown = ({ source }) => {
-  if(source === null) return null;
+  if (source === null) return null;
   return processor.processSync(source).result;
 };
 
